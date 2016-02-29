6f0fa63445ac390ad00cd905c28b6708f92bcc1d
Amy Liao <amyhliao88@gmail.com>
2016-02-28 17:44:46 -0800

    add width and height vairables

---
+++ js/game.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  var canvas = $("#canvas")[0];
+  var contex = canvas.getContext("2d");
+  var width = $("#canvas").width();
+  var height = $("#canvas").height();
+
+});
