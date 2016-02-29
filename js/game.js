c53298c2e8e57e5b4cb4791a899c81fb1a658ce6
Amy Liao <amyhliao88@gmail.com>
2016-02-28 17:59:06 -0800

    add food, score, array, and cell width variables

--- js/game.js
+++ js/game.js
@@ -3,5 +3,8 @@ $(document).ready(function() {
   var contex = canvas.getContext("2d");
   var width = $("#canvas").width();
   var height = $("#canvas").height();
-
+  var cellWidth = 10;
+  var food;
+  var score;
+  var snakeArray;
 });
